8cc7f2c16cf86fd331e787ee5cf2ea4ea70a0142
daikon510 <d-sone@cnt.biglobe.co.jp>
2019-01-08 18:57:38 +0900

    FindOdd

--- src/main/java/com/codewars/_6kyu/FindOdd/FindOdd.java
+++ src/main/java/com/codewars/_6kyu/FindOdd/FindOdd.java
@@ -1,19 +1,19 @@
-package com.codewars._6kyu.FindTheUniqueNumber;
+package com.codewars._6kyu.FindOdd;
 
 import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Kata {
-    public static double findUniq(double arr[]) {
-        Map<Double, Long> map = Arrays.stream(arr)
+public class FindOdd {
+    public static int findIt(int[] a) {
+        Map<Integer, Long> map = Arrays.stream(a)
                 .sorted()
                 .boxed()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-        for (Map.Entry<Double, Long> tmp : map.entrySet()) {
-            if (tmp.getValue() != 1) {
+        for (Map.Entry<Integer, Long> tmp : map.entrySet()) {
+            if (tmp.getValue()%2 != 0) {
                 return tmp.getKey();
             }
         }
